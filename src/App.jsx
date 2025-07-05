8223a3f27c9cc660d83929a2ecae5d5f7d2afd57
Лев Ширенин <shirenin.ln@gmail.com>
2025-07-05 11:58:10 +0300

    html

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <>
-      <h1 className="text-6xl text-gray-500">Hello, World!</h1>
-    </>
+    <div className="container mx-auto">
+      <header className="flex justify-between items-center mb-6 mt-2 border-b border-gray-500 py-2 mx-2">
+        <h1 className="text-2xl font-bold text-gray-100">Копилка</h1>
+        <div className="flex gap-3">
+          <input type="text" className="bg-red-50 rounded px-1" placeholder="Найти"></input>
+        </div>
+      </header>
+    </div>
   )
 }
 
